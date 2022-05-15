295c01119d3864e984b4c6ba90e34e49df4d2d92
holero <nothing>
2022-05-15 12:32:25 -0700

    ditto

--- build.sh
+++ build.sh
@@ -5,35 +5,49 @@
 # Creatred On: 5/15/2022
 
 
-# Variables
-USERNAME = $1
-SSHSERVER = $2
-LISTENPORT = $3
-IMAGENAME = $4
-
 DATE=$(date -u +%Y%m%d-%H.%M.%S)
 LOG="/tmp/server.log"
 
-echo "<${DATE}><INFO>_Start Build ${IMAGENAME}" >> ${LOG} 2>&1
+# Check to make sure you have input your variables
 
-# Create ssh keypair, no passphrase, no Comment
-
-ssh-keygen -t rsa -b 4096 -f "./${IMAGENAME}" -q -N '' -C '' >> ${LOG} 2>&1
-
-# Build Image Using DockerFile
-
-docker build ./ -t $IMAGENAME  \
---build-arg ssh_user="${USERNAME}" \
---build-arg ssh_server="${SSHSERVER}" \
---build-arg listen_port="${LISTENPORT}" \
---build-arg image_name="${IMAGENAME}" \
->> ${LOG} 2>&1
+if [] && [] && [] && []; then
 
 
-if [ $? -ne 0 ]; then
-  echo "<${DATE}><ERROR>_Please Verify Config" >> ${LOG} 2>&1
-  exit 1
+  # Variables
+  USERNAME = $1
+  SSHSERVER = $2
+  LISTENPORT = $3
+  IMAGENAME = $4
+
+
+  echo "<${DATE}><INFO>_Start Build ${IMAGENAME}" >> ${LOG} 2>&1
+
+  # Create ssh keypair, no passphrase, no Comment
+
+  ssh-keygen -t rsa -b 4096 -f "./${IMAGENAME}" -q -N '' -C '' >> ${LOG} 2>&1
+
+  # Build Image Using DockerFile
+
+  docker build ./ -t $IMAGENAME  \
+  --build-arg ssh_user="${USERNAME}" \
+  --build-arg ssh_server="${SSHSERVER}" \
+  --build-arg listen_port="${LISTENPORT}" \
+  --build-arg image_name="${IMAGENAME}" \
+  >> ${LOG} 2>&1
+
+
+  if [ $? -ne 0 ]; then
+    echo "<${DATE}><ERROR>_Please Verify Config" >> ${LOG} 2>&1
+    exit 1
+  else
+    echo "<${DATE}><INFO>_Built, Please copy ssh public key to remote ssh socks server (${SSHSERVER})" >> ${LOG} 2>&1
+    exit 0
+  fi
+
 else
-  echo "<${DATE}><INFO>_Built, Please copy ssh public key to remote ssh socks server (${SSHSERVER})" >> ${LOG} 2>&1
-  exit 0
+
+  echo "<${DATE}><ERROR>_Please Specify Correct Arguments" >> ${LOG} 2>&1
+  echo "<${DATE}><ERROR>_Please Specify Correct Arguments"
+  echo "<${DATE}><Info>_build.sh USERNAME SSHSERVER LISTENPORT IMAGENAME"
+
 fi
